4a7c1d22202b79f235a808052abcc67eedeb135a
Sergey Lokot <sergey.lokot@gmail.com>
2016-06-02 13:05:21 -0400

    Update heap sort

--- SLokot.Sorting/Heap.cs
+++ SLokot.Sorting/Heap.cs
@@ -10,41 +10,160 @@ namespace SLokot.Sorting
     {
         public static void Sort(long[] A)
         {
-            int L = A.Length;
-            for (int k = 2; k <= L; k += 1)
-            {
-                int i = k, j = k >> 1;
-                long c = A[i - 1], p;
-                while (j >= 1 && c > (p = A[j - 1]))
-                {
-                    A[i - 1] = p;
-                    i = j;
-                    j >>= 1;
-                }
-                A[i - 1] = c;
-            }
+            if (A == null) return;
 
-            for (int l = L; l > 1; l -= 1)
+            int L = A.Length;
+            if (L < 2) return;
+
+            if (L == 2)
             {
-                int i = 1, j = 2, k = 3;
-                long c = A[l - 1], s; A[l - 1] = A[0];
-                while (true)
+                if (A[0] > A[1])
                 {
-                    if (k < l && (s = A[k - 1]) > A[j - 1] && s > c)
-                    {
-                        A[i - 1] = s;
-                        i = k;
-                    }
-                    else if (j < l && (s = A[j - 1]) > c)
-                    {
-                        A[i - 1] = s;
-                        i = j;
-                    }
-                    else break;
-                    j = i << 1;
-                    k = j + 1;
+                    long c = A[0];
+                    A[0] = A[1];
+                    A[1] = c;
                 }
-                A[i - 1] = c;
+            }
+            else if (L == 3)
+            {
+                long l = A[0];
+                long m = A[1];
+                long r = A[2];
+
+                if (l > r)
+                {
+                    if (m > l)
+                    {
+                        A[0] = r;
+                        A[1] = l;
+                        A[2] = m;
+                    }
+                    else if (m < r)
+                    {
+                        A[0] = m;
+                        A[1] = r;
+                        A[2] = l;
+                    }
+                    else
+                    {
+                        A[0] = r;
+                        A[2] = l;
+                    }
+                }
+                else
+                {
+                    if (m > r)
+                    {
+                        A[1] = r;
+                        A[2] = m;
+                    }
+                    else if (m < l)
+                    {
+                        A[0] = m;
+                        A[1] = l;
+                    }
+                }
+            }
+            else if (L < 16)
+            {
+                int pos = 0;
+                long min = A[0];
+                for (int k = 1; k < L; k += 1)
+                {
+                    if (A[k] < min)
+                    {
+                        min = A[k];
+                        pos = k;
+                    }
+                }
+                if (pos > 0)
+                {
+                    A[pos] = A[0];
+                    A[0] = min;
+                }
+
+                for (int k = 2; k < L; k += 1)
+                {
+                    int i = k, j = k - 1;
+                    long c = A[i], p;
+                    while (c < (p = A[j]))
+                    {
+                        A[i] = p;
+                        i = j;
+                        j -= 1;
+                    }
+                    A[i] = c;
+                }
+            }
+            else
+            {
+                int pos = 0;
+                long max = A[0];
+                for (int k = 1; k < L; k += 1)
+                {
+                    if (A[k] > max)
+                    {
+                        max = A[k];
+                        pos = k;
+                    }
+                }
+                if (pos > 0)
+                {
+                    A[pos] = A[0];
+                    A[0] = max;
+                }
+
+                for (int k = 4; k <= L; k += 1)
+                {
+                    int i = k, j = k >> 1;
+                    long c = A[i - 1], p;
+                    while (c > (p = A[j - 1]))
+                    {
+                        A[i - 1] = p;
+                        i = j;
+                        j >>= 1;
+                    }
+                    A[i - 1] = c;
+                }
+
+                for (int l = L; l > 4; l -= 1)
+                {
+                    int i = 1, j = 2, k = 3;
+                    long c = A[l - 1], s; A[l - 1] = A[0];
+                    while (true)
+                    {
+                        if (k < l && (s = A[k - 1]) > A[j - 1] && s > c)
+                        {
+                            A[i - 1] = s;
+                            i = k;
+                        }
+                        else if (j < l && (s = A[j - 1]) > c)
+                        {
+                            A[i - 1] = s;
+                            i = j;
+                        }
+                        else break;
+                        j = i << 1;
+                        k = j + 1;
+                    }
+                    A[i - 1] = c;
+                }
+
+                long t = A[3];
+                A[3] = A[0];
+                A[0] = t;
+
+                t = A[2];
+                if (t < A[1])
+                {
+                    A[2] = A[1];
+                    if (t < A[0])
+                    {
+                        A[1] = A[0];
+                        A[0] = t;
+                    }
+                    else A[1] = t;
+                }
             }
         }
     }
